934ce3d3b9f39068ced5b66529789be762957158
Carlos Santamaria <alexrace18@gmail.com>
2023-12-27 18:20:52 -0600

    Primer commit

---
+++ TareasMVC/Migrations/20231220041829_AdminRol.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TareasMVC.Migrations
+{
+    /// <inheritdoc />
+    public partial class AdminRol : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+                        IF NOT EXISTS(SELECT Id FROM AspNetRoles where Id='0d5f3113-f47e-449b-b047-d7c3ffade6a2')
+                        BEGIN
+	                        INSERT AspNetRoles(Id,[Name],[NormalizedName])
+	                        VALUES ('0d5f3113-f47e-449b-b047-d7c3ffade6a2','admin','ADMIN')
+                        END");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DELETE AspNetRoles WHERE Id='0d5f3113-f47e-449b-b047-d7c3ffade6a2'");
+        }
+    }
+}
